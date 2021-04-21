5a0a7bc1d699f2abe8e6fa1826a5caec6312935f
Deg-var <gogpop1229@gmail.com>
2021-04-21 19:51:21 +0500

    fon

--- catalog_db.php
+++ catalog_db.php
@@ -1,34 +1,35 @@
 <?php
-function get_products(){
-    return[
-        "1" =>[
+function get_products()
+{
+    return [
+        "1" => [
             "title" => "DEATH ZONE",
             "img_kart" => "img/deth zone.png",
             "opis" => "Королевская битва. Зоны закрываются по таймеру. Мигрируй в центр если сможешь.",
             "ssil" => "https://www.youtube.com/embed/Cgpn_nYYiBw",
         ],
-        "2" =>[
+        "2" => [
             "title" => "DEATH ZONE WITH ZONEE",
             "img_kart" => "img/deth zone2.png",
             "opis" => "Королевская битва. Зоны закрываются по таймеру. Теперь идти в центр сразу
             нельзя. зоны открываются по таймеру. для миграции каждый раз есть по 300 секунд.",
             "ssil" => "https://www.youtube.com/embed/Cgpn_nYYiBw",
         ],
-        "3" =>[
+        "3" => [
             "title" => "CAPTURE THE COW",
             "img_kart" => "img/COW.png",
             "opis" => "Захват коровы это как захват флага только вместо флага корова. Кто приведет
             корову себе на базу 3 раза",
             "ssil" => "https://www.youtube.com/embed/I2x77taaZzE",
         ],
-        "4" =>[
+        "4" => [
             "title" => "GEOMETRIA",
             "img_kart" => "img/geo.png",
             "opis" => "Абсолютная геометричность. Все игроки разделены утесами. Можно идти через
             центр, а можно и через задный",
             "ssil" => "0",
         ],
-        "5" =>[
+        "5" => [
             "title" => "MYASORUBKA",
             "img_kart" => "img/MYASO.png",
             "opis" => "Чтобы победить надо удержать монумент 50 лет. У всех есть все военные здания и
@@ -36,14 +37,14 @@ function get_products(){
             обеспечивают ресурсами.",
             "ssil" => "https://www.youtube.com/embed/Wf13Kt0ZKHs",
         ],
-        "6" =>[
+        "6" => [
             "title" => "KINGS & PINGWINS",
             "img_kart" => "img/ping.png",
             "opis" => "Пингвины сильные, живут в антарктиде и всегда голодны. Скорми им 20 крестьян и
             они перейдут на твою сторону.",
             "ssil" => "https://www.youtube.com/embed/lZ3l3dmSCqY",
         ],
-        "7" =>[
+        "7" => [
             "title" => "LOVE THE KING",
             "img_kart" => "img/KING.png",
             "opis" => "В нашем королестве завелся бешеный король. Он бегает вокруг лабиринта и
@@ -51,52 +52,63 @@ function get_products(){
             в друзья. Но его потеря равно поражение.",
             "ssil" => "0",
         ],
-        "8" =>[
+        "8" => [
             "title" => "VSA KARTA ETO KYRGAN",
             "img_kart" => "img/45.jpg",
             "opis" => "Мой город идеален как карта для эпохи, ведь уровень инфраструктуры застрял
             где-то в 13 веке.",
             "ssil" => "0",
         ],
-        "9" =>[
+        "9" => [
             "title" => "FOREST OF WONDERS",
             "img_kart" => "img/forest.png",
             "opis" => "Королевство пришло в упадок, но ходят слухи что в центре земель есть лес
             изобилующими богатвами. Найди их чтобы победить своих врагов.",
             "ssil" => "https://www.youtube.com/embed/FTn-_47aTYE",
         ],
-        "10" =>[
+        "10" => [
             "title" => "KINGS RACING",
             "img_kart" => "img/KR.jpg",
             "opis" => "Добро пожаловать на гонки королей. Чтобы ускорить своего короля строй осадные
             башни. Чей король придет первым тот и победил.",
             "ssil" => "https://www.youtube.com/embed/H9KEPXm6aVM",
         ],
-        "11" =>[
+        "11" => [
             "title" => "KINGS RACING WITH DEATH",
             "img_kart" => "img/KR2.jpg",
             "opis" => "То же самое что и первый вариант но с одним дополнительным условием. Если игрок
             потеряет всех крестьян и все ратуши он так же проигрывает.",
             "ssil" => "https://www.youtube.com/embed/H9KEPXm6aVM",
         ],
-
-        ];
+        "12" => [
+            "title" => "4 Seasons",
+            "img_kart" => "img/4.jpg",
+            "opis" => "Перезжаем по кольцу. 4 зоны. В каждой можно находиться до 20 минут. 
+            После все юниты погибнут в ней. На переход в следуюшую зону 5 минут. Монахи не умирают в закрытых зонах.",
+            "ssil" => "https://www.youtube.com/embed/ikz3E7zsohM",
+        ],
+    ];
 }
-function get_product_attribute ($id, $attr){
+function get_product_attribute($id, $attr)
+{
     $products = get_products();
     $result = $products[$id][$attr] ?? null;
     return $result;
 }
 
-function get_product_title($id){
-    return get_product_attribute($id, "title")
-;}
-function get_product_img_kart($id){
-    return get_product_attribute($id, "img_kart")
-;}
-function get_product_opis($id){
-    return get_product_attribute($id, "opis")
-;}
-function get_product_ssil($id){
-    return get_product_attribute($id, "ssil")
-;}
+function get_product_title($id)
+{
+    return get_product_attribute($id, "title");
+}
+function get_product_img_kart($id)
+{
+    return get_product_attribute($id, "img_kart");
+}
+function get_product_opis($id)
+{
+    return get_product_attribute($id, "opis");
+}
+function get_product_ssil($id)
+{
+    return get_product_attribute($id, "ssil");
+}
--- index.php
+++ index.php
@@ -7,28 +7,29 @@
     <title>Обзор модов</title>
 
 </head>
+
 <body>
-<header>
-    <div class="glagne">
-        <nav>
-            <div class="padloshka">
-                <a href="#autor">
-                    ОБ АВТОРЕ </a> &emsp;•&emsp;
-                <a href="#karta">
-                    КАРТЫ</a>&emsp;•&emsp;
-                <a href="#svas">
-                    ОБРАТНАЯ СВЯЗЬ
-                </a>
-            </div>
-            <div class="chtoto">ВЕСЕЛЫЕ КАРТЫ ДЛЯ <br>AGE OF EMPIRES II: DEFINITIVE EDITION</div>
-        </nav>
-    </div>
-</header>
+    <header>
+        <div class="glagne">
+            <nav>
+                <div class="padloshka">
+                    <a href="#autor">
+                        ОБ АВТОРЕ </a> &emsp;•&emsp;
+                    <a href="#karta">
+                        КАРТЫ</a>&emsp;•&emsp;
+                    <a href="#svas">
+                        ОБРАТНАЯ СВЯЗЬ
+                    </a>
+                </div>
+                <div class="chtoto">ВЕСЕЛЫЕ КАРТЫ ДЛЯ <br>AGE OF EMPIRES II: DEFINITIVE EDITION</div>
+            </nav>
+        </div>
+    </header>
 
 
     <div class="citata"><q>Бред как рабочая стратегия</q><br><em> — wanderbraun</em></div>
     <div id="autor" class="autor">
-        <div><img class="contimg" src="img/i.jpg"></div>
+        <div><img class="contimg" src="img/no.png"></div>
         <div class="cont">
             <div class="cont2">
                 <p>ОБ АВТОРЕ</p>
@@ -46,29 +47,29 @@
         $karts = get_products();
         require("karta.php");
         ?>
-        <div class="contKarts"><?= $count_karts?></div>
+        <div class="contKarts"><?= $count_karts ?></div>
     </div>
-        <div class="svas" id="svas">
-            <div class="svas_text">Есть предложения?<br>Свяжись со мной!</div>
-            <a href="https://discord.gg/86J2dS8Gk2"><img class="svas_img" src="img/dis.png" alt="Discord"></a>
-            <a href="https://t.me/Degvar"><img class="svas_img" src="img/telega.png"></a>
-            <a href="https://vk.com/deg_var"><img class="svas_img" src="img/vk.png"></a>
-        </div>
-        <div id="openModal" class="modal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h3 class="modal-title">Название</h3>
-                        <a href="#close" title="Close" class="close">×</a>
-                    </div>
-                    <div class="video"><iframe width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                    <div class="modal-body">
-                        <p>Содержимое модального окна...</p>
-                    </div>
+    <div class="svas" id="svas">
+        <div class="svas_text">Есть предложения?<br>Свяжись со мной!</div>
+        <a href="https://discord.gg/86J2dS8Gk2"><img class="svas_img" src="img/dis.png" alt="Discord"></a>
+        <a href="https://t.me/Degvar"><img class="svas_img" src="img/telega.png"></a>
+        <a href="https://vk.com/deg_var"><img class="svas_img" src="img/vk.png"></a>
+    </div>
+    <div id="openModal" class="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title">ЗАКРОЙ МЕНЯ</h3>
+                    <a href="#close" title="Close" class="close">×</a>
+                </div>
+                <div class="video"><iframe width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div class="modal-body">
+                    <p>Позязя</p>
                 </div>
             </div>
         </div>
-        <script src="js.js"></script>
-        <script src="knopke.js"></script>
-        <footer>© Александр Мурашев 2021</footer>
+    </div>
+    <script src="js.js"></script>
+    <script src="knopke.js"></script>
+    <footer>© Александр Мурашев 2021</footer>
 </body>
